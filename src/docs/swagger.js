0dd05f54a48c6e5ebb9d5f713c04f5fbc5d80d95
Joaomachado19 <joaovwmachado@gmail.com>
2025-04-03 21:15:17 -0300

    Resolvido erro com o swagger ui

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,45 +1,46 @@
 const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Gestão de Consultas Médicas',
-            version: '1.0.0',
-        },
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT'
-                }
-            },
-            schemas: {
-                Paciente: {
-                    type: 'object',
-                    properties: {
-                        id: { type: 'integer' },
-                        nome: { type: 'string' },
-                        data_nascimento: { type: 'string', format: 'date' },
-                        cpf: { type: 'string' },
-                        telefone: { type: 'string' },
-                        email: { type: 'string', format: 'email' },
-                        historico: { type: 'string' },
-                        convenio: { type: 'string' }
-                    }
-                }
-            }
-        }
-    },
-    apis: ['./src/routes/*.js']
+  definition: {
+      openapi: '3.0.0',
+      info: {
+          title: 'Gestão de Consultas Médicas',
+          version: '1.0.0',
+      },
+      components: {
+          securitySchemes: {
+              bearerAuth: {
+                  type: 'http',
+                  scheme: 'bearer',
+                  bearerFormat: 'JWT'
+              }
+          },
+          schemas: {
+              Paciente: {
+                  type: 'object',
+                  properties: {
+                      id: { type: 'integer' },
+                      nome: { type: 'string' },
+                      data_nascimento: { type: 'string', format: 'date' },
+                      cpf: { type: 'string' },
+                      telefone: { type: 'string' },
+                      email: { type: 'string', format: 'email' },
+                      historico: { type: 'string' },
+                      convenio: { type: 'string' }
+                  }
+              }
+          }
+      }
+  },
+  apis: ['./src/routes/*.js']
 };
 
 const specs = swaggerJsDoc(options);
 
 module.exports = (app) => {
-    app.use('/api-docs',
-        swaggerUi.serve,
-        swaggerUi.setup(specs, { explorer: true })
-    );
+  app.use('/api-docs',
+      swaggerUi.serve,
+      swaggerUi.setup(specs, { explorer: true })
+  );
 };
